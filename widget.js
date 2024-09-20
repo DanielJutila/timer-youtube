0ac05d2ff282c4351b1e8b74b9d565f6700abd4f
Daniel Jutila <daniel.jutila@gmail.com>
2024-09-20 18:29:31 -0500

    Started widget.js

---
+++ widget.js
@@ -0,0 +1,7 @@
+let userLocale = "en-US",
+    // should be 5 minutes by default
+    newFollower = 5.0,
+    newSubscriber = 5.0,
+    newMember = 5.0,
+    minDonation = 1.0,
+    newDonation = 5.0;
