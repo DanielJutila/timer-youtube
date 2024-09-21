23247eeee414ad44f16e57291b40fdfa871d682a
Daniel Jutila <daniel.jutila@gmail.com>
2024-09-21 13:04:06 -0500

    events work properly now

--- widget.js
+++ widget.js
@@ -1,24 +1,30 @@
 let userLocale = "en-US",
-    // should be 5 minutes by default
-    newSubscriber = 5,
-    newMember = 5,
-    minDonation = 1.0,
-    newDonation = 5;
+    newMember = 5;
 
 window.addEventListener("onEventReceived", function (obj) {
     if(!obj.detail.event) return;
-    if(typeof obj.detail.event.itemId !== "undefined"){
-
-        const listener = obj.detail.listener.split("-"[0]);
-        const event = obj.detail.event;
-        let widget = document.getElementById("main-container");
+    if(typeof obj.detail.event._id !== "undefined"){
+        //I added addEvent here and it was not triggered
+        const listener = obj.detail.listener;
+        console.log(listener);
+   
         if(listener === "sponsor-latest"){
-            widget.innerHTML = `<h1>Recent member</h1><p>HELLO</p>`;
+            addEvent("red");
+        } else {
+          
         }
     };
 
 });
 
+
+function addEvent(color) {
+    let widget = document.getElementById("main-container");
+    let element = document.createElement('div');
+    element.className = 'event';
+    element.innerHTML = `<p style='background-color: ${color};'>New Subscriber</p>`;
+    widget.appendChild(element);
+}
 
 // function addTime(time) {
 //     time = time * 60;
