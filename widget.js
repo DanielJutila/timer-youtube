3870be54f43569bce6ab8efaa18116dbdd319160
Daniel Jutila <daniel.jutila@gmail.com>
2024-10-02 15:56:28 -0500

    added widget spinnig clock

--- widget.js
+++ widget.js
@@ -2,6 +2,8 @@ let userLocale = "en-US";
 
 const socket = new WebSocket("ws://localhost:8080");
 let timer;
+let interval;
+let animation = false;
 const connectWebSocket = () => {
   socket.onopen = () => {
     console.log("Connected to WebSocket");
@@ -16,7 +18,9 @@ const connectWebSocket = () => {
     let message = JSON.parse(event.data);
     if (message.type === "time") {
       timer = message.data;
-      updateCountdown();
+      if(!animation){
+        updateCountdown(timer);
+      }
     }
   };
   socket.onerror = (error) => {
@@ -45,6 +49,7 @@ window.addEventListener("onEventReceived", function (obj) {
         timeToAdd = 1800;
       }
       if(timeToAdd > 0){
+        animateIncrease(timer, timeToAdd);
         socket.send(JSON.stringify({ type: "tier", data: timeToAdd }));
       }
     } else {
@@ -55,10 +60,37 @@ window.addEventListener("onEventReceived", function (obj) {
   }
 });
 
-function updateCountdown() {
-  let hours = Math.floor(timer / 3600);
-  let minutes = Math.floor((timer % 3600) / 60);
-  let seconds = timer % 60;
+function animateIncrease(currentTime, timeToAdd) {
+  clearInterval(interval);
+  if (timeToAdd === 0) return;
+
+  const targetTime = currentTime + timeToAdd;
+  const animationDuration = 500;
+  const steps = 200;
+  const stepInterval = animationDuration / steps; 
+  const timePerStep = timeToAdd / steps; 
+  
+  let currentStep = 0;
+
+  if (currentTime < targetTime) {
+    animation = true;
+    interval = setInterval(() => {
+      if (currentStep >= steps || currentTime >= targetTime) {
+        clearInterval(interval);
+        animation = false;
+        updateCountdown(targetTime); 
+      } else {
+        currentTime += timePerStep;
+        currentStep++;
+        updateCountdown(Math.floor(currentTime));
+      }
+    }, stepInterval);
+  }
+}
+function updateCountdown(time) {
+  let hours = Math.floor(time / 3600);
+  let minutes = Math.floor((time % 3600) / 60);
+  let seconds = time % 60;
 
   minutes = minutes < 10 ? "0" + minutes : minutes;
   seconds = seconds < 10 ? "0" + seconds : seconds;
@@ -69,8 +101,9 @@ function updateCountdown() {
   }
   const p = document.createElement("p");
   p.textContent = `${hours}:${minutes}:${seconds}`;
+  p.classList.add("countdownText");
   countdownDiv.appendChild(p);
-  if (--timer < 0) {
+  if (--time < 0) {
     clearInterval(interval);
   }
 }
